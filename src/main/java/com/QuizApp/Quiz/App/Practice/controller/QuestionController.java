1fc9f6ecb1a1d2bc4eec973a35ea51d090038756
Abir <abirhasan.supta254@gmail.com>
2024-08-04 20:15:14 +0600

    Exception Handle

--- src/main/java/com/QuizApp/Quiz/App/Practice/controller/QuestionController.java
+++ src/main/java/com/QuizApp/Quiz/App/Practice/controller/QuestionController.java
@@ -1,10 +1,9 @@
 package com.QuizApp.Quiz.App.Practice.controller;
 
-
 import com.QuizApp.Quiz.App.Practice.model.Question;
 import com.QuizApp.Quiz.App.Practice.service.QuestionService;
-import org.aspectj.weaver.patterns.TypePatternQuestions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,29 +16,32 @@ public class QuestionController {
     QuestionService questionService;
 
     @GetMapping("allQuestions")
-    public List<Question> getAllQuestions(){
+    public ResponseEntity<List<Question>> getAllQuestions(){
         return questionService.getAllQuestions();
+    }
+    @GetMapping("{id}")
+    public ResponseEntity<?> getQuestionById(@PathVariable int id){
+        return questionService.getQuestionById(id);
     }
 
     @GetMapping("category/{category}")
-    public List<Question> getQuestionsByCategory(@PathVariable String category){
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("add")
-    public String addQuestion(@RequestBody Question q){
+    public ResponseEntity<String> addQuestion(@RequestBody Question q){
         return questionService.addQuestion(q);
     }
 
     @DeleteMapping("delete/{id}")
-    public String deleteQuestion(@PathVariable int id){
+    public ResponseEntity<String> deleteQuestion(@PathVariable int id){
         return questionService.deleteQuestion(id);
     }
 
     @PutMapping("update/{id}")
-    public String updateQuestion(@PathVariable int id, @RequestBody Question q){
+    public ResponseEntity<String> updateQuestion(@PathVariable int id, @RequestBody Question q){
         return questionService.updateQuestion(id, q);
     }
-
 
 }
--- src/main/java/com/QuizApp/Quiz/App/Practice/service/QuestionService.java
+++ src/main/java/com/QuizApp/Quiz/App/Practice/service/QuestionService.java
@@ -3,9 +3,13 @@ package com.QuizApp.Quiz.App.Practice.service;
 import com.QuizApp.Quiz.App.Practice.dao.QuestionDB;
 import com.QuizApp.Quiz.App.Practice.model.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestionService {
@@ -14,38 +18,84 @@ public class QuestionService {
     QuestionDB questionDB;
 
 
-    public List<Question> getAllQuestions() {
-        return questionDB.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try{
+            return new ResponseEntity<>(questionDB.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public List<Question> getQuestionsByCategory(String category) {
-        return questionDB.findByCategory(category);
+    public ResponseEntity<?> getQuestionById(int id){
+        try {
+            if (questionDB.existsById(id)) {
+                Optional<Question> question = questionDB.findById(id);
+                return new ResponseEntity<>(question, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>("Question with this id does not exist.", HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("An error occurred while processing the request.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
-    public String addQuestion(Question q) {
-        questionDB.save(q);
-        return "Success";
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try{
+            return new ResponseEntity<>(questionDB.findByCategory(category), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
     }
 
-    public String deleteQuestion(int id) {
-        questionDB.deleteById(id);
-        return "Success";
+    public ResponseEntity<String> addQuestion(Question q) {
+        try{
+            questionDB.save(q);
+            return new ResponseEntity<>("Success", HttpStatus.CREATED);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Failed", HttpStatus.BAD_REQUEST);
+
+    }
+
+    public ResponseEntity<String> deleteQuestion(int id) {
+        if(questionDB.existsById(id)){
+            try {
+                questionDB.deleteById(id);
+                return new ResponseEntity<>("Success", HttpStatus.OK);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return new ResponseEntity<>("Failed", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("Question with this id does not exist.", HttpStatus.NOT_FOUND);
+
     }
 
 
-    public String updateQuestion(int id, Question q) {
+    public ResponseEntity<String> updateQuestion(int id, Question q) {
         Question existingQuestion = questionDB.findById(id).orElse(null);
         if(existingQuestion != null){
-            existingQuestion.setQuestion(q.getQuestion());
-            existingQuestion.setOption1(q.getOption1());
-            existingQuestion.setOption2(q.getOption2());
-            existingQuestion.setOption3(q.getOption3());
-            existingQuestion.setCategory(q.getCategory());
-            existingQuestion.setDifficulty(q.getDifficulty());
-            existingQuestion.setRightAns(q.getRightAns());
-            questionDB.save(existingQuestion);
-            return "Success";
+            try{
+                existingQuestion.setQuestion(q.getQuestion());
+                existingQuestion.setOption1(q.getOption1());
+                existingQuestion.setOption2(q.getOption2());
+                existingQuestion.setOption3(q.getOption3());
+                existingQuestion.setCategory(q.getCategory());
+                existingQuestion.setDifficulty(q.getDifficulty());
+                existingQuestion.setRightAns(q.getRightAns());
+                questionDB.save(existingQuestion);
+                return new ResponseEntity<>("Success", HttpStatus.OK);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return new ResponseEntity<>("Failed", HttpStatus.BAD_REQUEST);
         }
-        return "Question Does Not Exist";
+        return new ResponseEntity<>("Question with this id does not exist.", HttpStatus.NOT_FOUND);
     }
 }
